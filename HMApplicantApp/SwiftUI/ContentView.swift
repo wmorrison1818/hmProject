9ecd9dbd35cbe1cfbb121c730305c7c35d793ee8
William Morrison <wmmhyc@mail.missouri.edu>
2022-07-10 20:11:03 -0500

    first changes

---
+++ HMApplicantApp/SwiftUI/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  HMApplicantApp
+//
+//  Created by happyMedium on 7/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Label("Hello, World", image: "appleIcon")
+    }
+    
+}
