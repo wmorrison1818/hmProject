d186ae0dbc6082ba56586ed6e5807b4f79f30327
William Morrison <wmmhyc@mail.missouri.edu>
2022-07-11 23:08:21 -0500

    Add early nav and bottom tool bar

---
+++ HMApplicantApp/UIKit/DummyViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DummyTableViewController.swift
+//  HMApplicantApp
+//
+//  Created by William Morrison on 7/10/22.
+//
+
+import Foundation
+import UIKit
+
+// TODO: Probably not worth my time to make this an actual table view controller.
+// I used a tableViewController because that is the default view that comes with a navigationController.
+class DummyTableViewController: UITableViewController {
+    var shoppingList: [String] = ["Men's boots"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Setup back button.
+        // TODO: Get custom back button working.
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        
+        let backButtonImage = UIImage(named: "arrow back")
+        self.navigationController?.navigationBar.backIndicatorImage = backButtonImage
+        self.navigationController?.navigationBar.backIndicatorTransitionMaskImage = backButtonImage
+    }
+}
--- HMApplicantApp/UIKit/ViewController.swift
+++ HMApplicantApp/UIKit/ViewController.swift
@@ -8,27 +8,17 @@
 import UIKit
 import SwiftUI
 
+// TODO: Add a tableviewcontroller with a variety of reusable cells (ie photo scroll) as shown in the demo.
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        addContentView()
     }
     
-    func addContentView() {
-        let contentView: ContentView = ContentView()
-        let hostingViewController: UIHostingController = .init(rootView: contentView)
-        hostingViewController.view.translatesAutoresizingMaskIntoConstraints = false
-        self.addChild(hostingViewController)
-        self.view.addSubview(hostingViewController.view)
-        
-        NSLayoutConstraint.activate([
-            view.leadingAnchor.constraint(equalTo: hostingViewController.view.leadingAnchor),
-            view.trailingAnchor.constraint(equalTo: hostingViewController.view.trailingAnchor),
-            view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: hostingViewController.view.safeAreaLayoutGuide.bottomAnchor),
-            view.safeAreaLayoutGuide.topAnchor.constraint(equalTo: hostingViewController.view.safeAreaLayoutGuide.topAnchor)
-        ])
+    // Hide the home indicator.
+    // TODO: See if this is a plist option.
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        return true
     }
 
 }
